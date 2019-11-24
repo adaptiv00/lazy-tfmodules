5981961514f1fb0f7a9d8a93c3082c33bb36a771
dvaida <31284076+dvaida@users.noreply.github.com>
2019-11-24 08:22:32 +0200

    Add no newlines option for Linux/non MacOS

--- modules/lazy-topology/bin/wrapper.sh
+++ modules/lazy-topology/bin/wrapper.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+BASE64_OPTS=""
+if [ "$(uname)" != "Darwin" ]; then
+    BASE64_OPTS="-w 0"
+fi
+
 # Terraform cannot read complex structures directly from external data sources
 # So we base64 encode, and then jsondecode(base64decode) and it seems to work
 DIR=$(dirname "$0")
 SCRIPTPATH="$( cd "$DIR" ; pwd -P )"
 "$SCRIPTPATH"/lazy-topology
-echo "{\"topology\":\"$( cat deploy/topology.json | base64 )\"}"
+echo "{\"topology\":\"$( cat deploy/topology.json | base64 $BASE64_OPTS )\"}"
